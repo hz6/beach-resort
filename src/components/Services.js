d394945b0a1e6f9feb66c934855e71133503b431
hz6 <hz6@ualberta.ca>
2020-12-03 11:23:54 -0700

    formatting

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
 import Title from './Title'
-import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons/fa'
-export default class  extends Component {
-    state={
-        services:[
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa'
+export default class extends Component {
+    state = {
+        services: [
             {
-                icon:<FaCocktail />,
-                title:'Free cocktails',
-                info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
+                icon: <FaCocktail />,
+                title: 'Free cocktails',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
             },
             {
-                icon:<FaHiking />,
-                title:'Fantastic hiking',
-                info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
+                icon: <FaHiking />,
+                title: 'Fantastic hiking',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
             },
             {
-                icon:<FaShuttleVan />,
-                title:'Free shuttle pickup',
-                info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
+                icon: <FaShuttleVan />,
+                title: 'Free shuttle pickup',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
             },
             {
-                icon:<FaBeer />,
-                title:'Wonderful beer',
-                info:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
+                icon: <FaBeer />,
+                title: 'Wonderful beer',
+                info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, corporis!'
             },
         ]
     }
     render() {
         return (
             <section className="services">
-                <Title title="services"/>
+                <Title title="services" />
                 <div className="services-center">
-                    {this.state.services.map((item,index)=>{
+                    {this.state.services.map((item, index) => {
                         return (
                             <article key={index} className='services'>
                                 <span>{item.icon}</span>
@@ -41,7 +41,7 @@ export default class  extends Component {
                         );
                     })}
                 </div>
-            </section >
+            </section>
         )
     }
 }
